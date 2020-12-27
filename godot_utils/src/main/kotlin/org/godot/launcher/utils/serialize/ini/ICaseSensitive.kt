2eaed41c3666891715466584b26b6990528d3430
Ushiosan23 <haloleyendee@outlook.com>
2020-12-26 20:35:58 -0600

    feat(kotlin): Scan ".ini" files from java easily.

---
+++ godot_utils/src/main/kotlin/org/godot/launcher/utils/serialize/ini/ICaseSensitive.kt
@@ -0,0 +1,13 @@
+package org.godot.launcher.utils.serialize.ini
+
+/**
+ * Interface to manage case sensitive objects
+ */
+interface ICaseSensitive {
+
+	/**
+	 * Check if is case sensitive
+	 */
+	var isCaseSensitive: Boolean
+
+}
