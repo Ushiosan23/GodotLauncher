658d3139b8fd391ca20d24bd3dc745c421c03ba5
Ushiosan23 <haloleyendee@outlook.com>
2021-02-02 19:10:18 -0600

    refactor(restart): :cry: Restart project

--- godot_utils/src/main/kotlin/org/godot/launcher/utils/server/ServerEngine.kt
+++
@@ -1,7 +0,0 @@
-package org.godot.launcher.utils.server
-
-data class ServerEngine(
-	val gd_id: Int,
-	val gd_name: String,
-	val gd_url: String
-)
--- settings.gradle.kts
+++
@@ -1,3 +0,0 @@
-rootProject.name = "Godot Launcher"
-include(":godot_utils")
-include(":app")
